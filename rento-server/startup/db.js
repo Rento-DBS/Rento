1ba1e2bba5ef7d1c3023e0b5438075699adfea98
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-05-19 18:31:51 +0530

    Changed db config

--- rento-server/startup/db.js
+++ rento-server/startup/db.js
@@ -2,7 +2,7 @@ const msssql = require("mssql");
 
 const config = {
     user: "SA",
-    password: "Cromastone7",
+    password: "Rento280101",
     server: "localhost",
     database: "TutorialDB",
     options: {
