7119fa51be9403be49cbc68426224e59ce5fccca
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-05-23 12:29:52 +0530

    designed login and register page

--- rento-client/src/components/Login/Login.jsx
+++ rento-client/src/components/Login/Login.jsx
@@ -1,22 +1,41 @@
 import React, { useState } from "react";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import { Paper } from "@material-ui/core";
+import { Button, TextField, withStyles} from "@material-ui/core/";
+import styles from "./Login.module.css";
 
-// const useStyles = makeStyles((theme) => ({
-//     root: {
-//         "& > *": {
-//             margin: theme.spacing(2),
-//             width: "25ch",
-//             display: "block",
-//         },
-//     },
-// }));
+const InputField = withStyles({
+    root: {
+      '& label.Mui-focused': {
+        color: 'rgb(28, 194, 16)',
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: 'rgb(28, 194, 16)',
+      },
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: 'rgb(28, 194, 16)',
+        },
+        '&:hover fieldset': {
+          borderColor: 'rgb(28, 194, 16)',
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: 'rgb(28, 194, 16)',
+        },
+      },
+    },
+  })(TextField);
 
-function SignIn() {
+
+function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    return <div></div>;
+    return (
+        <form className={styles.container}>
+            <h1 className={styles.heading}>Login</h1>
+            <InputField name="email" type="email" className={styles.textfield} required label="Email ID" variant="outlined"  />
+            <InputField name="password" type="password" className={styles.textfield} required label="Password" variant="outlined" />
+            <Button className={styles.button} >Login</Button>
+        </form>
+    );
 }
 
-export default SignIn;
+export default Login;
--- rento-client/src/components/Register/Register.jsx
+++ rento-client/src/components/Register/Register.jsx
@@ -1,55 +1,41 @@
 import React, { useState } from "react";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import { Paper } from "@material-ui/core";
-import { blue } from "@material-ui/core/colors";
+import { Button, TextField, withStyles} from "@material-ui/core/";
+import styles from "./Register.module.css";
 
-function Register() {
-    const styles = {
-        width: 300,
-        lineHeight: 4,
-        position: "absolute",
-        top: "20%",
-        right: "30%",
-    };
+const InputField = withStyles({
+    root: {
+      '& label.Mui-focused': {
+        color: 'rgb(28, 194, 16)',
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: 'rgb(28, 194, 16)',
+      },
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: 'rgb(28, 194, 16)',
+        },
+        '&:hover fieldset': {
+          borderColor: 'rgb(28, 194, 16)',
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: 'rgb(28, 194, 16)',
+        },
+      },
+    },
+  })(TextField);
 
-    // const classes = useStyles();
+
+function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     return (
-        <div style={styles}>
-            <Paper style={{ height: 350, backgroundColor: blue }} elevation={3}>
-                <h1
-                    style={{
-                        fontSize: 32,
-                        height: 80,
-                    }}
-                >
-                    Register
-                </h1>
-                <form>
-                    <TextField
-                        id='outlined-basic'
-                        label='Email'
-                        variant='outlined'
-                    />
-                    <TextField
-                        id='outlined-basic'
-                        label='Password'
-                        variant='outlined'
-                    />
-                    <Button
-                        style={{ width: 100 }}
-                        variant='outlined'
-                        color='primary'
-                        fullWidth={false}
-                    >
-                        Register
-                    </Button>
-                </form>
-            </Paper>
-        </div>
+        <form className={styles.container}>
+            <h1 className={styles.heading}>Register</h1>
+            <InputField name="email" type="email" className={styles.textfield} required label="Email ID" variant="outlined"  />
+            <InputField name="password" type="password" className={styles.textfield} required label="Password" variant="outlined" />
+            <Button className={styles.button} variant="outlined" >Register</Button>
+        </form>
     );
 }
 
-export default Register;
+export default Login;
